c2554171cb42a1f212eac6dbdae8d04bb76838de
arrdem <rmckenzie92@gmail.com>
2013-04-15 00:55:33 -0500

    quick writeup

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject me.arrdem.imprecise "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject me.arrdem.imprecise "0.1.0"
+  :description "a toleranced arithmetic library for clojure"
+  :url "http://github.com/arrdem/imprecise"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
