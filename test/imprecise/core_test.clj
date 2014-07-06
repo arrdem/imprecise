f0d0ef7b87d18518695594af7f4c69e169e8db5d
arrdem <me@arrdem.com>
2014-07-06 17:43:06 -0500

    Start reworking towards 1.0.0

---
+++ test/imprecise/core_test.clj
@@ -0,0 +1,19 @@
+(ns me.arrdem.imprecise-test
+  (:require [clojure.test :refer :all]
+            [clojure.algo.generic
+             [arithmetic :refer [+ - / *]]
+             [comparison :as c]
+             [math-functions :refer :all]]
+            [imprecise.core :refer :all]))
+
+
+(deftest arithmetic-tests
+  )
+
+
+(deftest multiplicative-tests
+  )
+
+
+(deftest comparison-tests
+  )
